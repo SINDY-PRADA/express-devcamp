55f43bd72fbe39277a40cba9f2b3a2fabed987e1
Your Name <you@example.com>
2022-10-27 09:31:21 -0500

    ruta de usuarios

--- config/db.js
+++ config/db.js
@@ -1,27 +1,34 @@
 const sequelize = require('./seq')
 const colors = require('colors')
-const { DataTypes } = require('sequelize')
 
-//Crear una instancia de el modelo User
-const UserModel = require('../models/user')
-const User= UserModel(sequelize, DataTypes)
+//const { DataTypes} =require('sequelize')
+//crea una instancia de el modelo user
+//const UserModel = require('../models/user')
+//const User =  UserModel(sequelize,DataTypes)
+//definir la funcion de conexion a la base 
+//de datos 
 
-    //definir la funcion de conexion a la base de datos
-    const connectDB = async ()=>{
-
-    //Conectarse a la base de datos
-    try {
+const connectDB = async ()=>{
+ //conectarse a la bd 
+ try{
     await sequelize.authenticate()
-    console.log('conectado mysql'.bgMagenta.blue)
-    const jane = await User.create(
-        { username: "Jane", 
-        email: "Jane@misena.edu.co", 
-        password:"7890" 
-        });
-    console.log("Jane's auto-generated ID:", jane.id);
-    
-    } catch (error){
+    console.log('conectado a mysql'.bgCyan.bgBlack)
+
+ }catch(error){
     console.log(error)
-    }
+ }
+
 }
-connectDB()
+module.exports = connectDB
+
+
+
+
+
+
+
+   /* const jane = await User.create
+    ({ username: "Jane", 
+       email: "ydcufino@misena.edu,.co",
+       password:"23456"});
+    console.log("Jane's auto-generated ID:", jane.id);*/
